4d8296aaaab5c162bd196deead739f8ee12bc142
Mikhail Gashenko <dr.psycho@live.com>
2018-06-25 01:01:34 +0600

    Managing non-memory resources using smart pointers

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
-#include <boost/intrusive_ptr.hpp>
-#include <boost/smart_ptr/intrusive_ref_counter.hpp>
+#include <memory>
+#include <ctime>
 
-namespace NS {
-  class Bar : public boost::intrusive_ref_counter<Bar, boost::thread_safe_counter> {
-   public:
-    Bar() {}
-    ~Bar() { std::cout << "~Bar invoked" << '\n'; }
-  };
-} // end NS
+struct FILEDeleter {
+  void operator()(FILE *fp) const {
+    fprintf(stderr, "Deleter invoked\n");
+    if (fp) {
+      fclose(fp);
+    }
+  }
+};
 
 int main() {
+  std::shared_ptr<FILE> spfile(fopen("tmp.txt", "a+"), FILEDeleter());
 
-  // Wrap dynamically-allocated objects. Pass true for the Boolean second
-  // argument so that the intrusive_ptr constructor increments Bar's refcount_
-  // through a call to intrusive_ptr_add_ref(NS::Bar*)
-  boost::intrusive_ptr<NS::Bar> pi(new NS::Bar, true);
-  boost::intrusive_ptr<NS::Bar> pi2(pi);
-  assert(pi.get() == pi2.get());
-  std::cout << "pi : " << pi.get() << '\n'
-            << "pi2: " << pi2.get() << '\n';
+  time_t t;
+  time(&t);
 
-  assert(pi->use_count() == pi2->use_count() && pi2->use_count() == 2);
-  std::cout << "pi->use_count() : " << pi->use_count() << '\n'
-            << "pi2->use_count() : " << pi2->use_count() << '\n';
+  if (spfile) {
+    fprintf(spfile.get(), "tstamp: %s\n", ctime(&t));
+  }
 
   return EXIT_SUCCESS;
 }
