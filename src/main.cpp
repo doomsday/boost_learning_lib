478dc754fd93c90578f442d2bde3f46ef8d79224
Mikhail Gashenko <dr.psycho@live.com>
2018-08-06 00:12:44 +0600

    boost::contains

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,9 @@
 #include <cassert>
 
 int main() {
-  std::string song = "Green-tinted sixties mind";
-  using RangeType = boost::iterator_range<std::string::iterator>;
-  RangeType range = boost::make_iterator_range(song.begin() + 13, song.begin() + 20);
-  boost::to_upper(range);
-  assert(song == "Green-tinted SIXTIES mind");
+  std::string input = "linearize";
+  std::string test = "near";
+  assert(boost::contains(input, test));
 
   return EXIT_SUCCESS;
 }
