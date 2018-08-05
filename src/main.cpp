7087b7b9762e62e14f83091b4e0e3081094a2845
Mikhail Gashenko <dr.psycho@live.com>
2018-08-06 00:27:02 +0600

    boost::find_first, boost::find_nth

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <string>
 #include <boost/algorithm/string.hpp>
 #include <cassert>
+#include <iostream>
 
 int main() {
-  std::string input = "linearize";
-  std::string test = "near";
-  assert(boost::contains(input, test));
+  const char *haystack = "Mary had a little lamb";
+  const char *needles[] = {"little", "Little", nullptr};
+
+  for (int i = 0; needles[i] != nullptr; ++i) {
+    auto ret = boost::find_first(haystack, needles[i]);
+//    auto ret = boost::find_nth(haystack, needles[i], 1);
+
+    if (ret.begin() == ret.end()) {
+      std::cout << "String [" << needles[i] << "] not found in"
+                << " string [" << haystack << "]\n";
+    } else {
+      std::cout << "String [" << needles[i] << "] found at "
+                << "offset " << ret.begin() - haystack
+                << " in string [" << haystack << "]\n";
+    }
+  }
 
   return EXIT_SUCCESS;
 }
