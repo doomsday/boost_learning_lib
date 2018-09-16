e8c7ff018097b16557c1bd115f8281f8af7b92ee
Mikhail Gashenko <dr.psycho@live.com>
2018-09-16 23:49:40 +0600

    boost::find

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,22 @@
 #include <iostream>
 #include <vector>
 
-struct min_len {
-  bool operator()(const std::string& s) const {
-    return s.size() > 3;
-  }
-};
+template<typename RangeT, typename NeedleT>
+boost::iterator_range<typename boost::range_iterator<RangeT>::type> find_xdigit_token(RangeT &input, NeedleT &needle) {
+  return boost::find(input, boost::last_finder(needle, boost::is_iequal()));
+}
 
 int main() {
-  std::string str = "The application tried to read from an invalid address at 0xbeeffed";
+  std::string str = "How little is too little1", needle = "Little";
+  boost::iterator_range<std::string::iterator> token = find_xdigit_token(str, needle);
 
-  std::vector<std::string> v;
-  auto ret = boost::iter_find(v, str, boost::token_finder(boost::is_xdigit(), boost::token_compress_on));
-
-  std::ostream_iterator<std::string> osit(std::cout, ", ");
-  std::copy_if(v.begin(), v.end(), osit, min_len());
+  while (token.begin() != token.end()) {
+    if (boost::size(token) > 3) {
+      std::cout << "\'" << token << "\' at position: " << token.begin() - str.begin() << '\n';
+    }
+    boost::iterator_range<std::string::iterator> remnant = boost::make_iterator_range(token.end(), str.end());
+    token = find_xdigit_token(remnant, needle);
+  }
 
   return EXIT_SUCCESS;
 }
