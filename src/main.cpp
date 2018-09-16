3bd7ea5380127769fc3a5d4095649ce099065775
Mikhail Gashenko <dr.psycho@live.com>
2018-09-16 21:23:39 +0600

    boost::find_token

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <string>
 #include <boost/algorithm/string.hpp>
-#include <cassert>
 #include <iostream>
 
+template<typename RangeT>
+boost::iterator_range<typename boost::range_iterator<RangeT>::type> find_xdigit_token(RangeT &Input) {
+  return boost::find_token(Input, boost::is_xdigit(), boost::token_compress_on);
+}
+
 int main() {
-  using string_range = boost::iterator_range<std::string::const_iterator>;
+  std::string str = "The application tried to read from an invalid address at 0xbeeffed";
+  boost::iterator_range<std::string::iterator> token = find_xdigit_token(str);
 
-  std::vector<string_range> matches;
-  std::string str = "He deserted the unit while they trudged through the desert one night.";
-
-  boost::find_all(matches, str, "desert");
-  for (auto match : matches) {
-    std::cout << "Found [desert] at offset " << match.begin() - str.begin() << ".\n";
+  while (token.begin() != token.end()) {
+    if (boost::size(token) > 3) {
+      std::cout << "\'" << token << "\' at position: " << token.begin() - str.begin() << '\n';
+    }
+    boost::iterator_range<std::string::iterator> remnant = boost::make_iterator_range(token.end(), str.end());
+    token = find_xdigit_token(remnant);
   }
 
   return EXIT_SUCCESS;
