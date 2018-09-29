0406bd1d18cf76ce91b8739e87579883d4d0cd8c
Mikhail Gashenko <dr.psycho@live.com>
2018-09-29 23:47:04 +0600

    boost::tokenizer Tokenizing based on separators

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,34 @@
 #include <string>
 #include <iostream>
-#include <vector>
 
-#include <boost/algorithm/string.hpp>
-
-bool five_or_less_chars(const std::string& s) { return s.size() <= 5; }
+#include <boost/tokenizer.hpp>
 
 int main() {
 
+  using std::string;
+
   try {
 
-    /**
-     * boost::join and boost::join_if algorithms are pretty useful when you are
-     * trying to string together a sequence of values with some separator between
-     * successive values.
-     */
+    string input = "God knows, I've never been a spiritual man!";
+    boost::tokenizer<> tokenized(input);
 
-    std::vector<std::string> vec {"mongrel", "puppy", "whelp", "hound"};
-    std::cout << boost::join_if(vec, ", ", five_or_less_chars);
+    /*
+     * Default tokenizing policy splits the string using whitespace and punctuation
+     * as token delimiters or separators. */
+    for(const string & token : tokenized) {
+      std::cout << token << '\n';
+    }
+
+    std::cout << '\n' << "******************" << "\n\n";
+
+    /*
+     * Split the input based on spaces and punctuation but not split on an apostrophe */
+    boost::char_separator<string::value_type> sep(" \t,.!?;./\"(){}[]<>");  // construct the splitting policy
+    using tokenizer = boost::tokenizer<boost::char_separator<char>>;
+    tokenizer my_tokenizer(input, sep);
+    for (const string & token : my_tokenizer) {
+      std::cout << token << '\n';
+    }
 
   } catch (const std::exception &e) {
     std::cout << e.what();
