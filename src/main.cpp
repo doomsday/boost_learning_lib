33407eff718590e4e837624b48969aa727fc5673
Mikhail Gashenko <dr.psycho@live.com>
2018-11-17 03:36:39 +0600

    Tokenizing records with fxed-length felds

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,16 @@
 
 int main() {
 
-  /**
-   * Tokenizing records with fields containing metacharacters.
-   */
-
   using std::string;
 
   try {
 
-    string input = R"(/Alon Ben-Ari/-35-11~/5 Zamenhoff St., Tel Aviv)";
-    // tilde (~) for escaping; hyphen (-) for field separator; forward slash (/) for quotes
-    boost::escaped_list_separator<char> sep('~', '-', '/');
-    using tokenizer = boost::tokenizer<boost::escaped_list_separator<string::value_type>>;
-    tokenizer my_tokenizer(input, sep);
+    string input = R"(201408091403290000001881303614419ABNANL2AWSSDEUTDEMM720000000412000EUR)";
+    int lengths[] = {8, 9, 16, 11, 11, 12, 13};
+
+    boost::offset_separator ofs(lengths, lengths + 7);
+    using tokenizer = boost::tokenizer<boost::offset_separator>;
+    tokenizer my_tokenizer(input, ofs);
 
     for (const string& tok : my_tokenizer) {
       std::cout << tok << '\n';
