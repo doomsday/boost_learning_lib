18fb96677e8c4065da654696385050be47f152d5
Mikhail Gashenko <dr.psycho@live.com>
2020-05-29 00:00:24 +0600

    Searching for substrings matching a regular expression

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <boost/regex.hpp>
 #include <string>
-#include <cassert>
 #include <iostream>
 
 int main() {
 
   using std::string;
+  using std::cout;
 
   try {
 
-    std::string str1 = "Alaska area";
-    boost::regex r1("a.*a");
-    assert(!boost::regex_match(str1, r1));
+    string haystack_str = "An array of papers from the academia on Alaska area's fauna";
+    boost::regex r2("a\\w*a");
+    boost::smatch matches;
+    string::const_iterator start = haystack_str.begin(), end = haystack_str.end();
+
+    while (boost::regex_search(start, end, matches, r2)) {
+      cout << "Matched substring " << matches.str()
+                << " at offset " << matches[0].first - haystack_str.begin()
+                << " of length " << matches[0].length() << '\n';
+      start = matches[0].second;  // set the start iterator to the first location past the end of the last match
+    }
 
   } catch (const std::exception &e) {
-    std::cout << e.what();
+    cout << e.what();
     return EXIT_FAILURE;
   }
 
