404a7f0e33d696c9cf07545afaa185876e13dbcd
Mikhail Gashenko <dr.psycho@live.com>
2020-06-04 23:07:57 +0600

    Parsing matching substrings and sub-expressions

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,23 @@ int main() {
   try {
 
     string haystack_str = "An array of papers from the academia on Alaska area's fauna";
-    boost::regex r2("a\\w*a");
+    boost::regex r2("a(\\w*)a");
     boost::smatch matches;
     string::const_iterator start = haystack_str.begin(), end = haystack_str.end();
 
     while (boost::regex_search(start, end, matches, r2)) {
-      cout << "Matched substring " << matches.str()
-                << " at offset " << matches[0].first - haystack_str.begin()
-                << " of length " << matches[0].length() << '\n';
-      start = matches[0].second;  // set the start iterator to the first location past the end of the last match
+      cout << "Matched substring [" << matches.str()
+           << "] following [" << matches.prefix().str()
+           << "] preceding [" << matches.suffix().str() << "]\n";
+      start = matches[0].second;
+      for (size_t s = 1; s < matches.size(); ++s) {
+        if (matches[s].matched) {
+          cout << "Matched substring [" << matches[s].str()
+               << "] at offset " << matches[s].first - haystack_str.begin()
+               << " of length " << matches[s].length() << '\n';
+        }
+      }
+      cout << std::endl;
     }
 
   } catch (const std::exception &e) {
