dfd9c4fff4049492ab7536d896eb2e33defd3bea
Mikhail Gashenko <dr.psycho@live.com>
2020-06-04 23:24:19 +0600

    Parsing strings using boost::regex_iterator

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <boost/regex.hpp>
-#include <string>
 #include <iostream>
+#include <string>
 
 int main() {
 
@@ -10,23 +10,11 @@ int main() {
   try {
 
     string haystack_str = "An array of papers from the academia on Alaska area's fauna";
-    boost::regex r2("a(\\w*)a");
-    boost::smatch matches;
-    string::const_iterator start = haystack_str.begin(), end = haystack_str.end();
+    boost::regex r1(R"(\ba\w*a\b)", boost::regex::icase);
+    boost::sregex_iterator rit(haystack_str.begin(), haystack_str.end(), r1), rend;
 
-    while (boost::regex_search(start, end, matches, r2)) {
-      cout << "Matched substring [" << matches.str()
-           << "] following [" << matches.prefix().str()
-           << "] preceding [" << matches.suffix().str() << "]\n";
-      start = matches[0].second;
-      for (size_t s = 1; s < matches.size(); ++s) {
-        if (matches[s].matched) {
-          cout << "Matched substring [" << matches[s].str()
-               << "] at offset " << matches[s].first - haystack_str.begin()
-               << " of length " << matches[s].length() << '\n';
-        }
-      }
-      cout << std::endl;
+    while (rit != rend) {
+      cout << *rit++ << '\n';
     }
 
   } catch (const std::exception &e) {
