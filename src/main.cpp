7e587797a0d7b18cf78e82064ba0850c80880233
Mikhail Gashenko <dr.psycho@live.com>
2020-06-08 01:48:08 +0600

    Movable but not copyable String

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
-#include <boost/regex.hpp>
+#include <boost/container/vector.hpp>
+#include <boost/foreach.hpp>
 #include <cassert>
 #include <iostream>
 
+#include "String.hpp"
+
 int main() {
 
-  try {
+  boost::container::vector<String> strVec;
+  String world("world");
+  // Move temporary (rvalue)
+  strVec.push_back(String("Hello"));
+  // Error, copy semantics needed
+  //strVec.push_back(world);
+  // Explicit move
+  strVec.push_back(boost::move(world));
+  // World nulled after move
+  assert(world.get() == nullptr);
+  // In-place construction
+  strVec.emplace_back("Hujambo Dunia!");  // Swahili
 
-    std::string str4 = "England's Queen, India's President, people's choice";
-    boost::regex r4(R"((\w+)'s\s+(\w+))");
-    std::string rep = boost::regex_replace(str4, r4, R"(\2 of \1)");
-    assert(rep == "Queen of England, President of India, choice of people");
-
-  } catch (const std::exception &e) {
-    std::cout << e.what();
-    return EXIT_FAILURE;
+  BOOST_FOREACH(auto& str, strVec) {
+    std::cout << str.get() << '\n';
   }
 
   return EXIT_SUCCESS;
