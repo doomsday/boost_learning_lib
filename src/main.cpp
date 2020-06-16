085820c2a2aa60268820887cc0a84c633c1c5be4
Mikhail Gashenko <dr.psycho@live.com>
2020-06-17 00:58:46 +0600

    Adding elements to the end of slist

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,19 @@
 
 int main() {
 
-  boost::container::slist<std::string> geologic_eras;
+  boost::container::slist<std::string> eras;
+  boost::container::slist<std::string>::iterator last = eras.before_begin();
 
-  geologic_eras.push_front("Neogene");
-  geologic_eras.push_front("Paleogene");
-  geologic_eras.push_front("Cretaceous");
-  geologic_eras.push_front("Jurassic");
-  geologic_eras.push_front("Triassic");
-  geologic_eras.push_front("Permian");
-  geologic_eras.push_front("Carboniferous");
-  geologic_eras.push_front("Devonian");
-  geologic_eras.push_front("Silurian");
-  geologic_eras.push_front("Ordovician");
-  geologic_eras.push_front("Cambrian");
+  const char *era_names [] = {"Cambrian", "Ordovician", "Silurian", "Devonian", "Carboniferous", "Silurian",
+                              "Devonian", "Carboniferous", "Cretaceous", "Paleogene", "Neogene"};
 
-  // Newest first
-  for (const auto& str : geologic_eras) {
-    std::cout << str << '\n';
+  for (const char *period : era_names) {
+    eras.emplace_after(last, period);
+    ++last;
+  }
+
+  for (const std::string& era : eras) {
+    std::cout << era << '\n';
   }
 
   return EXIT_SUCCESS;
