2cd5bc605be897344e8e747a4c3f2f58b605ae50
Mikhail Gashenko <dr.psycho@live.com>
2020-08-07 00:00:24 +0600

    Stable vector example

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,63 @@
 #include <string>
-#include <iostream>
-#include <vector>
+#include <boost/container/stable_vector.hpp>
 #include <cassert>
+#include <iostream>
+
+/* What we want to show is that once we add elements beyond the capacity of the
+ * stable_vector, requiring an expansion of the underlying array and movement of
+ * the existing data, iterators computed before the change in capacity still remain valid */
 
 int main() {
 
-  std::vector<int> v {1, 2, 3, 4, 5};
-  auto first = v.begin();
-  auto last = first + v.size() - 1;
-  assert(*last == 5);
-  v.insert(last, 4);  // inserting new element before the element at the specified position
-  // *last = 10 // undefined behavior, invalid iterator
-  for (int i = 0; i < 1000; ++i) {
-    /* Even appending an element to the end of the vector could trigger a resize
-     * of the vector's internal storage, requiring elements to be moved */
-    v.push_back(i);
+  const char *cloud_names[] = {"cumulus", "cirrus", "stratus", "cumulonimbus", "cirrostratus",
+                               "cirrocumulus", "altocumulus", "altostratus"};
+
+  boost::container::stable_vector<std::string> clouds;
+  clouds.reserve(4);
+  //clouds.resize(4); // To circumvent a bug in Boost 1.54
+
+  size_t name_count = sizeof(cloud_names)/sizeof(const char*);
+  size_t capacity = clouds.capacity();
+
+  size_t i = 0;
+  for (i = 0; i < name_count && i < capacity; ++i) {
+    clouds.push_back(cloud_names[i]);
+  }
+  std::cout << "\n1 ===========\n";
+  for (auto & cloud : clouds) {
+    std::cout << "\"" << cloud << "\"\n";
   }
 
-  // *first = 0;  // likely invalidated
+  auto first = clouds.begin();
+
+  for (; i < name_count; ++i) {
+    clouds.push_back(cloud_names[i]);
+  }
+  std::cout << "\n2 ===========\n";
+  for (auto & cloud : clouds) {
+    std::cout << "\"" << cloud << "\"\n";
+  }
+
+  auto sixth = clouds.begin() + 5;
+
+  // 1 erase @4
+  clouds.erase(clouds.begin() + 4);
+  std::cout << "\n3 ===========\n";
+  for (auto & cloud : clouds) {
+    std::cout << "\"" << cloud << "\"\n";
+  }
+  // 2 inserts @3
+  clouds.insert(clouds.begin() + 3, "stratocumulus");
+  clouds.insert(clouds.begin() + 3, "nimbostratus");
+
+  std::cout << "\n4 ===========\n";
+  for (auto & cloud : clouds) {
+    std::cout << "\"" << cloud << "\"\n";
+  }
+
+  assert(*first == cloud_names[0]);
+  assert(sixth == clouds.begin() + 6);  // not +5
+  assert(*sixth == cloud_names[5]);
 
   return EXIT_SUCCESS;
 
