5c683e0a4cda69630f2c077dc711746df14de29b
Mikhail Gashenko <dr.psycho@live.com>
2020-08-07 00:59:10 +0600

    Using a static_vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,26 @@
-#include <string>
-#include <boost/container/stable_vector.hpp>
-#include <cassert>
+#include <boost/current_function.hpp>
+#include <boost/container/static_vector.hpp>
 #include <iostream>
 
-/* What we want to show is that once we add elements beyond the capacity of the
- * stable_vector, requiring an expansion of the underlying array and movement of
- * the existing data, iterators computed before the change in capacity still remain valid */
+/* boost::container::static_vector template is a vector-like container with an
+ * upper limit on the size defined at compile time */
+
+class chatty_init {
+ public:
+  chatty_init() {
+    std::cout << BOOST_CURRENT_FUNCTION << '\n';
+  }
+};
 
 int main() {
 
-  const char *cloud_names[] = {"cumulus", "cirrus", "stratus", "cumulonimbus", "cirrostratus",
-                               "cirrocumulus", "altocumulus", "altostratus"};
-
-  boost::container::stable_vector<std::string> clouds;
-  clouds.reserve(4);
-  //clouds.resize(4); // To circumvent a bug in Boost 1.54
-
-  size_t name_count = sizeof(cloud_names)/sizeof(const char*);
-  size_t capacity = clouds.capacity();
-
-  size_t i = 0;
-  for (i = 0; i < name_count && i < capacity; ++i) {
-    clouds.push_back(cloud_names[i]);
-  }
-  std::cout << "\n1 ===========\n";
-  for (auto & cloud : clouds) {
-    std::cout << "\"" << cloud << "\"\n";
+  boost::container::static_vector<chatty_init,10> myvector;
+  std::cout << "boost::container::static_vector initialized" << '\n';
+  while (myvector.size() < myvector.capacity()) {
+    myvector.push_back(chatty_init());
   }
 
-  auto first = clouds.begin();
-
-  for (; i < name_count; ++i) {
-    clouds.push_back(cloud_names[i]);
-  }
-  std::cout << "\n2 ===========\n";
-  for (auto & cloud : clouds) {
-    std::cout << "\"" << cloud << "\"\n";
-  }
-
-  auto sixth = clouds.begin() + 5;
-
-  // 1 erase @4
-  clouds.erase(clouds.begin() + 4);
-  std::cout << "\n3 ===========\n";
-  for (auto & cloud : clouds) {
-    std::cout << "\"" << cloud << "\"\n";
-  }
-  // 2 inserts @3
-  clouds.insert(clouds.begin() + 3, "stratocumulus");
-  clouds.insert(clouds.begin() + 3, "nimbostratus");
-
-  std::cout << "\n4 ===========\n";
-  for (auto & cloud : clouds) {
-    std::cout << "\"" << cloud << "\"\n";
-  }
-
-  assert(*first == cloud_names[0]);
-  assert(sixth == clouds.begin() + 6);  // not +5
-  assert(*sixth == cloud_names[5]);
+  // myvector.push_back(chatty_init()); // std::bad_alloc
 
   return EXIT_SUCCESS;
 
